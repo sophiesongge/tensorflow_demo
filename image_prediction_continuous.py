54b3bdf3b8e31e4d5f2a77d1f73793436a07717a
sophiesongge <sophiesongge@gmail.com>
2017-06-05 10:32:10 +0200

    add continuous prediction

---
+++ image_prediction_continuous.py
@@ -0,0 +1,64 @@
+import numpy as np
+import tensorflow as tf
+import os
+import time
+
+imagePath = ''
+modelFullPath = '/tmp/output_graph.pb'
+labelsFullPath = '/tmp/output_labels.txt'
+
+
+def create_graph():
+    """Creates a graph from saved GraphDef file and returns a saver."""
+    # Creates graph from saved graph_def.pb.
+    with tf.gfile.FastGFile(modelFullPath, 'rb') as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+        _ = tf.import_graph_def(graph_def, name='')
+
+
+def run_inference_on_image():
+    answer = None
+
+    if not tf.gfile.Exists(imagePath):
+        tf.logging.fatal('File does not exist %s', imagePath)
+        return answer
+
+    image_data = tf.gfile.FastGFile(imagePath, 'rb').read()
+
+    # Creates graph from saved GraphDef.
+    create_graph()
+
+    with tf.Session() as sess:
+
+        softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+        predictions = sess.run(softmax_tensor,
+                               {'DecodeJpeg/contents:0': image_data})
+        predictions = np.squeeze(predictions)
+
+        top_k = predictions.argsort()[-5:][::-1]  # Getting top 5 predictions
+        f = open(labelsFullPath, 'rb')
+        lines = f.readlines()
+        labels = [str(w).replace("\n", "") for w in lines]
+        for node_id in top_k:
+            human_string = labels[node_id]
+            score = predictions[node_id]
+            print('%s (score = %.5f)' % (human_string, score))
+
+        answer = labels[top_k[0]]
+        return answer
+
+def detector(dir, sec):
+    origin = set([_f[2] for _f in os.walk(dir)][0])
+    time.sleep(sec)
+    final = set([_f[2] for _f in os.walk(dir)][0])
+    return final.difference(origin)
+
+
+if __name__ == '__main__':
+    while True:
+        fileset = detector("/tmp/input", 0.5)
+        if(len(fileset) != 0):
+            imagePath = "/tmp/input/"+fileset.pop()
+            print(imagePath)
+            run_inference_on_image()
