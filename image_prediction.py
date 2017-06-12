415b42b777f3244074ad2a775f1369699c65e9f8
sophiesongge <sophiesongge@gmail.com>
2017-06-12 16:50:23 +0200

    use opencv to write on the output image

--- image_prediction.py
+++ image_prediction.py
@@ -1,5 +1,6 @@
 import numpy as np
 import tensorflow as tf
+import cv2
 
 import getopt
 import sys
@@ -7,7 +8,7 @@ import sys
 imagePath = ''
 modelFullPath = '/tmp/output_graph.pb'
 labelsFullPath = '/tmp/output_labels.txt'
-
+outputPath = ''
 
 def create_graph():
     """Creates a graph from saved GraphDef file and returns a saver."""
@@ -30,6 +31,8 @@ def run_inference_on_image():
     # Creates graph from saved GraphDef.
     create_graph()
 
+    orig = cv2.imread(imagePath)
+
     with tf.Session() as sess:
 
         softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
@@ -38,7 +41,7 @@ def run_inference_on_image():
         predictions = np.squeeze(predictions)
 
         top_k = predictions.argsort()[-5:][::-1]  # Getting top 5 predictions
-        f = open(labelsFullPath, 'rb')
+        f = open(labelsFullPath, 'r')
         lines = f.readlines()
         labels = [str(w).replace("\n", "") for w in lines]
         for node_id in top_k:
@@ -46,12 +49,18 @@ def run_inference_on_image():
             score = predictions[node_id]
             print('%s (score = %.5f)' % (human_string, score))
 
-        answer = labels[top_k[0]]
+        answer = labels[top_k[0]].replace("\n", " ")
+        cv2.putText(orig, "Output label: {}".format(answer), (10, 30),
+    cv2.FONT_HERSHEY_SIMPLEX, 0.9, (102, 0, 255), 2)
+        prob = predictions[top_k[0]]
+        cv2.putText(orig, "Prob: {0:.0f}%".format(prob * 100), (10, 90), 
+    cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
+        cv2.imwrite(outputPath, orig)
         return answer
 
 
 if __name__ == '__main__':
-    opts,args = getopt.getopt(sys.argv[1:],'-i:-m:-l:',['imagePath=','modelFullPath=','labelsFullPath='])
+    opts,args = getopt.getopt(sys.argv[1:],'-i:-m:-l:-o:',['imagePath=','modelFullPath=','labelsFullPath=','outputPath='])
     for opt_name,opt_value in opts:
         if opt_name in ('-i','--imagePath'):
             imagePath = opt_value
@@ -59,5 +68,7 @@ if __name__ == '__main__':
             modelFullPath = opt_value
         if opt_name in ('-l','--labelsFullPath'):
             labelsFullPath = opt_value
+        if opt_name in ('-o','--outputPath'):
+            outputPath = opt_value
     run_inference_on_image()
 
